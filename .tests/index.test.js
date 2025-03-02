9951e98ac376a435d80c97a3519526c33dc6d92f
neil.ghosh <neil.ghosh@gmail.com>
2025-03-02 17:52:09 +0000

    fixed tests . test for api

--- .tests/index.test.js
+++ .tests/index.test.js
@@ -12,4 +12,26 @@ describe('Test the root path', () => {
         expect(response.statusCode).toBe(200);
         //console.log(response.data);
     }, 20000);
+
+
+});
+
+describe('Test the /api path', () => {
+    test('It should respond with 200 to POST method with image', async () => {
+        const response = await request(app)
+            .post('/api')
+            .attach('img', `${__dirname}/test.jpeg`);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('birdData');
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('imgBuffer');
+    }, 20000);
+
+    test('It should respond with 400 to POST method without image', async () => {
+        const response = await request(app)
+            .post('/api')
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error');
+
+    }, 20000);
 });
--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const path = require('path')
 const bodyParser = require('body-parser');
-var cors=require('cors');
+var cors = require('cors');
 
 
-const { setupGemini } = require('./lib/geminiSetup'); 
+const { setupGemini } = require('./lib/geminiSetup');
 const { schema } = require('./lib/schema');
 const { handleImage } = require('./lib/imageHandler');
 
 
 const app = express();
-app.use(cors({origin:true,credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -37,21 +37,19 @@ app.get('/', (req, res) => {
 
 
 app.post('/', async (req, res) => {
-  console.log(req.body);
   model = await setupGemini(projectId, apiKeyName, schema);
-  data = await handleImage(req, model);
+  data = await handleImage(req.files, model);
   res.render('index', data);
 });
 
 app.post('/api', async (req, res) => {
-  console.log(req.body);
-  model = await setupGemini(projectId, apiKeyName, schema);
-  data = await handleImage(req, model);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-
-  res.send(data);
+  if (!req.files) {
+    res.status(400).send({error: "No files uploaded"});
+  } else {
+    model = await setupGemini(projectId, apiKeyName, schema);
+    data = await handleImage(req.files, model);
+    res.send(data);
+  }
 });
 
 // (async () => {
